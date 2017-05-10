3f8dfd94f4e4acd5fec109cf4b549e2fb8bb1f13
darkblackart <zebrowski27200@student.pwsztar.edu.pl>
2017-05-10 10:52:59 +0200

    do errors

--- app/app.js
+++ app/app.js
@@ -1,30 +1,29 @@
+var joke = 'JavaScript walked into a bar....';
+var toldJoke = 'false';
+var $punch_line = 'Better watch out for those semi-colons.';
 
-var joke = "JavaScript walked into a bar....';
-var toldJoke = "false";
-var $punchline = 'Better watch out for those semi-colons.'
-var result
 
 if (toldJoke === true) {
-    Console.log($punchline);
+    console.log($punchline);
 } else {
-    console.log(joke);
+    console.log(joke;
 }
 
-var zip_code = 98104;
-var joesFavorite_Movie = Forbidden Planet;
+var forbiddenPlanet;
+var joesFavoriteMovie = forbiddenPlanet;
 var movieTicket$ = 9;
 //
 if (movieTicket$ >= 9) {
-    console.log("Too much!");
+    console.log('Too much!');
 } else {
-    console.log("We're going to see " + joesFavoriteMovie);
+    console.log('We\'re going to see ' + joes favoriteMovie);
 }
 
 var a = 5;
-var b = "5";
+var b = '5'
 var equal;
 
-if (a == b) {
+if (a == b)
     equal = true;
 } else {
     equal = false;
